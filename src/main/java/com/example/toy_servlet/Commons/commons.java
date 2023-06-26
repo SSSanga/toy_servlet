fd3db56411d790d16a212cfda7369d36e34cc7dd
SSSanga <ksa901125@gmail.com>
2023-06-26 19:01:24 +0900

    first commit

---
+++ src/main/java/com/example/toy_servlet/Commons/commons.java
@@ -0,0 +1,29 @@
+package com.example.toy_servlet.Commons;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+
+public class commons {
+    public Statement getStatement(){
+        String url = "jdbc:mysql://192.168.0.31:3306/db_polls";
+        String user = "yojulab";
+        String password ="!yojulab*";
+         Statement statement = null;
+        
+        try {
+            
+            Connection connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+
+            
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return statement;
+    }
+}
