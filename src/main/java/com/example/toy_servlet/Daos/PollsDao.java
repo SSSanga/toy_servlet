29ec6d85dd185155190033d04227769e9bdd27b7
SSSanga <ksa901125@gmail.com>
2023-06-27 12:13:54 +0900

    상_push: PollsDao에 Commons 연결

--- src/main/java/com/example/toy_servlet/Daos/PollsDao.java
+++ src/main/java/com/example/toy_servlet/Daos/PollsDao.java
@@ -1,5 +1,6 @@
 package com.example.toy_servlet.Daos;
 
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.example.toy_servlet.Commons.Commons;
@@ -10,12 +11,16 @@ import com.example.toy_servlet.Commons.Commons;
 
 
 public class PollsDao {
+    //db 연결 및 statement(=editor), String query까지 포함. 
     Commons commons = new Commons();
+    Statement statement = commons.getStatement();
+    String query = "";
     
     pulic ?? pollQuestion(){
         //질문과 답항을 불러오자
         // 1. 답을 불러오자_hashmap을 arraylist로 넣는다. 
         ArrayList arraylist = new ArrayList();
+
 
         try {
             
