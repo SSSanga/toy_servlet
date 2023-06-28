9f73d6e3dfb8d14f431ebdd7272bee139ad5a871
aabchyein <abcqaz369@naver.com>
2023-06-28 11:10:15 +0900

    23-06-28 혜인 통계

--- src/main/java/com/example/toy_servlet/controlls/StatisticsServlet.java
+++ src/main/java/com/example/toy_servlet/controlls/StatisticsServlet.java
@@ -1,10 +1,6 @@
 package com.example.toy_servlet.controlls;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,28 +9,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.yaml.snakeyaml.error.YAMLException;
-
 import com.example.toy_servlet.Daos.PollsDao;
 
-@WebServlet(urlPatterns = "/MembersServlet")
-public class MembersServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/StatisticsServlet")
+public class StatisticsServlet extends HttpServlet{
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
             PollsDao pollsDao = new PollsDao();
-            ArrayList List = new ArrayList();
-            List = pollsDao.SelectMembers();
-
+            int total = pollsDao.replyCount();
+            
+            request.setAttribute("TOTAL", total);  
 
             response.setContentType("text/html;charset=UTF-8");
             // 다음 파일 호출  
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/members.jsp");
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/statistics.jsp");
             requestDispatcher.forward(request, response);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-
+    
 }
--- src/main/resources/META-INF/resources/statistics.jsp
+++ src/main/resources/META-INF/resources/statistics.jsp
@@ -1,3 +1,5 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ page import="java.util.HashMap, java.util.ArrayList" %>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -25,7 +27,7 @@
         <br>
         <tbody>
             <tr>
-                <td>4</td>
+                <td><%= request.getAttribute("TOTAL") %> 명</td>
             </tr>
         </tbody>
 
