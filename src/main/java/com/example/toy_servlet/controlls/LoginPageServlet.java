c9ad6903e398244c2325ded720e210a9257bd178
SSSanga <ksa901125@gmail.com>
2023-06-28 11:11:21 +0900

    상_push: Survlet radio로 선택 가능하게 함. 제출은 불가

--- src/main/java/com/example/toy_servlet/controlls/LoginPageServlet.java
+++ src/main/java/com/example/toy_servlet/controlls/LoginPageServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "Login/Page")
+@WebServlet(urlPatterns = "/Login/Page")
 public class LoginPageServlet  extends HttpServlet {               
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {                                                         
